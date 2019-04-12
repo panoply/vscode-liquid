b579ec936cb498c3ec70ff52cb0d3f2dfcad801f
Nikolas Savvidis <nicos.savidis@gmail.com>
2019-04-12 17:32:41 +0200

    v1.5.0

--- extension/index.js
+++ extension/index.js
@@ -6,49 +6,73 @@ var vscode = require('vscode');
 var prettydiff = _interopDefault(require('prettydiff'));
 
 /**
- * # PrettyDiff Defaults
+ * PrettyDiff Defaults
  */
 const defaults = prettydiff.defaults;
 
 /**
- * # Editor Configuration
+ * Preset Configuration
  */
-const editor = vscode.workspace.getConfiguration('editor');
+const preset = {
+  tags: ['javascript',
+    'stylesheet',
+    'schema',
+    'style'],
+  ignore: [
+    'script', // <script>
+    'comment' // {% comment %}
+  ]
+};
 
 /**
- * Default Formatting Rules
+ * Editor Configuration
+ */
+const editor = vscode.workspace.getConfiguration('editor');
+const liquid = vscode.workspace.getConfiguration('liquid');
+
+/**
+ * Rules
  */
 const rules = {
   html: {
     mode: 'beautify',
+    language_name: 'Liquid',
     language: 'html',
     lexer: 'markup',
     fix: true,
+    preserve: 1,
     indent_size: editor.tabSize,
-    ignore_tags: ['script',
-      'style',
-      'comment']
+    end_quietly: 'log',
+    node_error: true
   },
   schema: {
     mode: 'beautify',
     language: 'JSON',
+    language_name: 'json',
     lexer: 'script',
     indent_size: editor.tabSize
   },
   stylesheet: {
     mode: 'beautify',
-    language: 'SCSS',
+    language_name: 'SASS',
+    language: 'scss',
     lexer: 'style',
+    preserve: 1,
     indent_size: editor.tabSize
   },
   javascript: {
     mode: 'beautify',
-    language: 'JavaScript',
+    language_name: 'JavaScript',
+    language: 'javascript',
     lexer: 'script',
+    preserve: 1,
     indent_size: editor.tabSize
   }
 };
 
+/**
+ * Command Palette
+ */
 const cmd = {
   document: 'liquid.formatDocument',
   selection: 'liquid.formatSelection',
@@ -56,159 +80,201 @@ const cmd = {
   disable: 'liquid.disableFormatting'
 };
 
-const tags = Object.keys(rules);
-const ignore = rules.html.ignore_tags;
-const items = tags.concat(ignore);
-
 var pattern = {
-  tags: items,
-  enforce: ['schema',
-    'style',
-    'stylesheet',
-    'javascript'],
-  inner: '((?:.|\\n)*?)',
-  close: '((?:</|{%-?)\\s*\\b(?:(?:|end)\\2)\\b\\s*(?:>|-?%}))',
-  ignored: new RegExp(`(<temp data-prettydiff-ignore>|</temp>)`, 'g'),
-  open: (tag) => `((?:<|{%-?)\\s*\\b(${tag})\\b(?:.|\\n)*?\\s*(?:>|-?%})\\s*)`,
-  ignore: (code) => `<temp data-prettydiff-ignore>${code}</temp>`,
-  matches () {
-    return new RegExp(this.open(this.tags.join('|')) + this.inner + this.close, 'g')
-  }
+  frontmatter: new RegExp(['---',
+    '(?:[^]*?)',
+    '---'].join(''), 'g'),
+  tags: new RegExp(
+    [
+      '(', // Opening
+      '(?:<|{%-?)\\s*',
+      `\\b(${preset.tags.concat(preset.ignore).join('|')})\\b`,
+      '(?:.|\\n)*?\\s*',
+      '(?:>|-?%})\\s*',
+      ')',
+      '(', // Inner
+      '(?:.|\\n)*?',
+      ')',
+      '(', // Closing
+      '(?:</|{%-?)\\s*',
+      '\\b(?:(?:|end)\\2)\\b',
+      '\\s*(?:>|-?%})',
+      ')'
+    ].join(''),
+    'g'
+  ),
+  ignore: new RegExp(['(',
+    '<temp data-prettydiff-ignore>',
+    '|',
+    '</temp>',
+    ')'].join(''), 'g')
 };
 
-let handler = {};
+class Format {
 
-const replace = (range, output) => [vscode.TextEdit.replace(range, output)];
-
-const blocks = (code, open, name, source, close) => {
-  if (pattern.enforce.includes(name) && open[0] === '{') {
-    let config = Object.assign({}, defaults, rules[name], { source });
-    let pretty = prettydiff.mode(config);
-    return pattern.ignore(`${open.trim()}\n\n${pretty.trim()}\n\n${close.trim()}`)
-  }
-  return pattern.ignore(`${code}`)
-};
-
-const fullRange = (document) => {
-  let first = document.lineAt(0).range.start.character;
-  let last = document.lineAt(document.lineCount - 1).range.end.character;
-  let range = new vscode.Range(0, first, document.lineCount - 1, last);
-
-  return range
-};
-
-function applyFormat (document, range) {
-  let contents = document.getText(range);
-  let source = contents.replace(pattern.matches(), blocks);
-  let assign = Object.assign({}, defaults, rules.html, {
-    source
-  });
-
-  let output = prettydiff.mode(assign);
-  output = output.replace(pattern.ignored, '');
-
-  return `${output.trim()}`
-}
-
-function format (document) {
-  let range = fullRange(document);
-  let output = applyFormat(document, range);
-
-  return replace(range, output)
-}
-
-function schema () {
-  let associate = vscode.workspace.getConfiguration('files').associations;
-  return {
-    scheme: 'file',
-    language: (associate && associate['*.liquid']) || 'liquid'
-  }
-}
-
-function disposal () {
-  if (Object.keys(handler).length > 0) {
-    handler.range.dispose();
-    handler.full.dispose();
-    handler = {};
-  }
-}
-
-const textDocument = vscode.commands.registerCommand(cmd.document, () => {
-  let document = vscode.window.activeTextEditor.document;
-  let range = fullRange(document);
-  let output = applyFormat(document, range);
-  vscode.window.activeTextEditor.edit((edits) => edits.replace(range, output));
-  vscode.window.showInformationMessage('Liquid Language: Document Formatted 💧');
-});
-
-const textSelection = vscode.commands.registerCommand(cmd.selection, () => {
-  let root = vscode.window.activeTextEditor;
-  let range = root.selection;
-  let output = applyFormat(root.document, range);
-  vscode.window.activeTextEditor.edit((edits) => edits.replace(range, output));
-  vscode.window.showInformationMessage('Liquid Language: Selection Formatted! 💧');
-});
-
-const formatEnable = vscode.commands.registerCommand(cmd.enable, () => {
-  let liquid = vscode.workspace.getConfiguration('liquid');
-  liquid.update('format', true, vscode.ConfigurationTarget.Global);
-  vscode.window.showInformationMessage('Liquid Language: Formatting Enabled 💧');
-});
-
-const formatDisable = vscode.commands.registerCommand(cmd.disable, () => {
-  let liquid = vscode.workspace.getConfiguration('liquid');
-  liquid.update('format', false, vscode.ConfigurationTarget.Global);
-  disposal();
-  vscode.window.showInformationMessage('Liquid Language: Formatting Disabled');
-});
-
-const registerFormat = () => {
-  if (!vscode.workspace.getConfiguration('liquid').format) {
-    return disposal()
+  static beautify (rule, source) {
+    const config = Object.assign({}, defaults, rules[rule], { source });
+    const pretty = prettydiff.mode(config);
+    return pretty
   }
 
-  if (!vscode.workspace.getConfiguration('editor').formatOnSave) {
-    return disposal()
+  static range (document) {
+    const all = document.getText();
+    return new vscode.Range(document.positionAt(0), document.positionAt(all.length - 1))
   }
 
-  if (Object.keys(handler).length > 0) {
-    disposal();
+  static ignore (code) {
+    return `<temp data-prettydiff-ignore>${code}</temp>`
   }
 
-  if (Object.keys(handler).length === 0) {
-    handler.full = vscode.languages.registerDocumentFormattingEditProvider(schema(), {
-      provideDocumentFormattingEdits: format
-    });
-    handler.range = vscode.languages.registerDocumentRangeFormattingEditProvider(schema(), {
-      provideDocumentRangeFormattingEdits: format
-    });
-  }
-};
-
-const registerRules = () => {
-  let liquid = vscode.workspace.getConfiguration('liquid');
-  pattern.tags.map((k) => {
-    if (liquid.beautify[k]) {
-      Object.assign(rules[k], liquid.beautify[k]);
+  static tags (code, open, name, source, close) {
+    if (preset.ignore.includes(name)) {
+      return Format.ignore(`${code.trim()}`)
     }
-  });
-};
+
+    const format = Format.beautify(name, source);
+    const output = `${open.trim()}\n\n${format.trim()}\n\n${close.trim()}`;
+    return Format.ignore(output.trim())
+  }
+
+  static code (document) {
+    if (document.match(pattern.frontmatter)) {
+      document = document.replace(pattern.frontmatter, Format.ignore);
+    }
+
+    const source = document.replace(pattern.tags, Format.tags);
+    const output = Format.beautify('html', source);
+    return output.replace(pattern.ignore, '')
+  }
+
+  static apply (document) {
+    const range = Format.range(document);
+    const result = Format.code(document.getText(range));
+    return {
+      range,
+      result
+    }
+  }
+
+  rules (liquid) {
+    preset.tags.map(language => {
+      if (liquid.beautify[language]) {
+        Object.assign(rules[language], liquid.beautify[language]);
+      }
+    });
+  }
+
+  register () {
+    vscode.languages.registerDocumentFormattingEditProvider(
+      {
+        scheme: 'file',
+        language: 'html'
+      },
+      {
+        provideDocumentFormattingEdits (document) {
+          const { range, result } = Format.apply(document);
+          return [vscode.TextEdit.replace(range, `${result.trim()}`)]
+        }
+      }
+    );
+  }
+
+  document () {
+    const { document } = vscode.window.activeTextEditor;
+    const { range, result } = Format.apply(document);
+    vscode.window.activeTextEditor.edit(code => code.replace(range, result));
+  }
+
+  selection () {
+    const { document, selection } = vscode.window.activeTextEditor;
+    const format = Format.code(document.getText(selection));
+    vscode.window.activeTextEditor.edit(code => code.replace(selection, format));
+  }
+
+}
+
+class Document extends Format {
+
+  static notify (message) {
+    return vscode.window.showInformationMessage(`Liquid: ${message}`)
+  }
+  constructor () {
+    super();
+    this.handler = null;
+    this.editor = vscode.window.activeTextEditor;
+    this.rules = super.rules(liquid);
+  }
+  dispose () {
+    if (this.handler !== null) {
+      this.handler.dispose();
+      this.handler = null;
+    }
+  }
+  format () {
+    if (!vscode.workspace.getConfiguration('liquid').format) {
+      return this.dispose()
+    }
+    if (!vscode.workspace.getConfiguration('editor').formatOnSave) {
+      return this.dispose()
+    }
+    this.dispose();
+    this.handler = super.register();
+  }
+  enable () {
+    liquid.update('format', true, vscode.ConfigurationTarget.Global);
+    return Document.notify('Formatting Enabled 💧')
+  }
+  disable () {
+    liquid.update('format', false, vscode.ConfigurationTarget.Global);
+    return Document.notify('Formatting Disabled')
+  }
+  selection () {
+    try {
+      super.selection();
+      Document.notify('Selection Formatted 💧');
+    } catch (error) {
+      Document.notify('Format Failed! The selection is invalid or incomplete!');
+    }
+  }
+  document () {
+    try {
+      super.document();
+      Document.notify('Document Formatted 💧');
+    } catch (error) {
+      Document.notify('Document could not be formatted, check your code!');
+    }
+  }
+
+}
 
 /**
  * # ACTIVATE EXTENSION
  */
-exports.activate = (context) => {
+exports.activate = context => {
   const active = vscode.window.activeTextEditor;
 
   if (!active || !active.document) return
 
-  registerRules();
-  registerFormat();
+  const document = new Document();
 
-  context.subscriptions.push(formatEnable);
-  context.subscriptions.push(formatDisable);
-  context.subscriptions.push(textDocument);
-  context.subscriptions.push(textSelection);
-  context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(registerFormat));
-  context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(registerFormat));
+  context.subscriptions.push(
+    vscode.workspace.onDidOpenTextDocument(() => {
+      document.format();
+    }),
+    vscode.workspace.onDidChangeConfiguration(() => {
+      document.format();
+    }),
+    vscode.commands.registerCommand(cmd.enable, () => {
+      document.enable();
+    }),
+    vscode.commands.registerCommand(cmd.disable, () => {
+      document.disable();
+    }),
+    vscode.commands.registerCommand(cmd.document, () => {
+      document.document();
+    }),
+    vscode.commands.registerCommand(cmd.selection, () => {
+      document.selection();
+    })
+  );
 };
