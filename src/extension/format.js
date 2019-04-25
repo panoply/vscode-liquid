362e3bacca37cff9da6717351bcc97f16213374e
Nikolas Savvidis <nicos.savidis@gmail.com>
2019-04-26 01:30:52 +0200

    Update format.js

--- src/extension/format.js
+++ src/extension/format.js
@@ -1,87 +1,154 @@
-import { languages, window, Range, TextEdit } from 'vscode'
-import { defaults, rules, preset } from './config'
+import { languages, window, TextEdit, Range } from 'vscode'
+import { preset, rules, ignore } from './config'
 import prettydiff from 'prettydiff'
 import pattern from './pattern'
 
 export default class Format {
 
+  /**
+   * @param {string} rule
+   * @param {string} source
+   */
   static beautify (rule, source) {
-    const config = Object.assign({}, defaults, rules[rule], { source })
-    const pretty = prettydiff.mode(config)
-    return pretty
+
+    prettydiff.options = Object.assign(prettydiff.options, rules[rule], {
+      source
+    })
+
+    return prettydiff()
+
   }
 
+  /**
+   * @param {object} document
+   */
   static range (document) {
-    const all = document.getText()
-    return new Range(document.positionAt(0), document.positionAt(all.length - 1))
+
+    const first = document.positionAt(0)
+    const last = document.positionAt(document.getText().length - 1)
+
+    return new Range(first, last)
+
   }
 
-  static ignore (code) {
+  /**
+   * @param {string} code
+   */
+  static ignores (code) {
+
     return `<temp data-prettydiff-ignore>${code}</temp>`
+
   }
 
+  /**
+   * @param {string} code
+   * @param {string} open
+   * @param {string} name
+   * @param {string} source
+   * @param {string} close
+   */
   static tags (code, open, name, source, close) {
-    if (preset.ignore.includes(name)) {
-      return Format.ignore(`${code.trim()}`)
+
+    if (ignore.includes(name)) {
+
+      return Format.ignores(`${code.trim()}`)
+
     }
 
     const format = Format.beautify(name, source)
     const output = `${open.trim()}\n\n${format.trim()}\n\n${close.trim()}`
-    return Format.ignore(output.trim())
+
+    return Format.ignores(output.trim())
+
   }
 
+  /**
+   * @param {object} document
+   */
   static code (document) {
+
     if (document.match(pattern.frontmatter)) {
-      document = document.replace(pattern.frontmatter, Format.ignore)
+
+      document = document.replace(pattern.frontmatter, Format.ignores)
+
     }
 
+    // Beautification
     const source = document.replace(pattern.tags, Format.tags)
     const output = Format.beautify('html', source)
-    return output.replace(pattern.ignore, '')
+    const result = output.replace(pattern.ignore, '')
+
+    return result
+
   }
 
+  /**
+   * @param {object} document
+   */
   static apply (document) {
+
     const range = Format.range(document)
     const result = Format.code(document.getText(range))
+
     return {
       range,
       result
     }
+
+  }
+
+  constructor () {
+
+    this.scheme = {
+      scheme: 'file',
+      language: 'html'
+    }
+
   }
 
   rules (liquid) {
-    preset.tags.map(language => {
+
+    preset.map(language => {
+
       if (liquid.beautify[language]) {
+
         Object.assign(rules[language], liquid.beautify[language])
+
       }
+
     })
+
   }
 
   register () {
-    return languages.registerDocumentFormattingEditProvider(
-      {
-        scheme: 'file',
-        language: 'html'
-      },
-      {
-        provideDocumentFormattingEdits (document) {
-          const { range, result } = Format.apply(document)
-          return [TextEdit.replace(range, `${result.trim()}`)]
-        }
+
+    return languages.registerDocumentFormattingEditProvider(this.scheme, {
+      provideDocumentFormattingEdits (document) {
+
+        const { range, result } = Format.apply(document)
+        return [ TextEdit.replace(range, `${result.trim()}`) ]
+
       }
-    )
+    })
+
   }
 
   document () {
+
     const { document } = window.activeTextEditor
     const { range, result } = Format.apply(document)
+
     window.activeTextEditor.edit(code => code.replace(range, result))
+
   }
 
   selection () {
+
     const { document, selection } = window.activeTextEditor
     const format = Format.code(document.getText(selection))
+
     window.activeTextEditor.edit(code => code.replace(selection, format))
+
   }
 
 }
