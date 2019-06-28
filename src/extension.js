e4c51c478eb23e57cab1bc944b323705918ebc52
Nikolas Savvidis <nicos.savidis@gmail.com>
2019-06-28 20:21:58 +0200

    bind subscriptions

--- src/extension.js
+++ src/extension.js
@@ -1,51 +1,27 @@
 import { window, workspace, commands } from 'vscode'
 import Document from './extension/document'
+import { output } from './extension/api'
+
+const { registerCommand } = commands
 
 /**
  * # ACTIVATE EXTENSION
  */
 exports.activate = context => {
 
+  const sub = context.subscriptions
   const active = window.activeTextEditor
 
   if (!active || !active.document) return
 
   const document = new Document()
 
-  context.subscriptions.push(
-    workspace.onDidOpenTextDocument(() => {
-
-      document.format()
-
-    })
-  )
-  context.subscriptions.push(
-    commands.registerCommand('liquid.disableFormatting', () => {
-
-      document.disable()
-
-    })
-  )
-  context.subscriptions.push(
-    commands.registerCommand('liquid.enableFormatting', () => {
-
-      document.enable()
-
-    })
-  )
-  context.subscriptions.push(
-    commands.registerCommand('liquid.formatDocument', () => {
-
-      document.document()
-
-    })
-  )
-  context.subscriptions.push(
-    commands.registerCommand('liquid.formatSelection', () => {
-
-      document.selection()
-
-    })
-  )
+  sub.push(workspace.onDidOpenTextDocument(document.format.bind(document)))
+  sub.push(workspace.onDidChangeConfiguration(document.init.bind(document)))
+  sub.push(registerCommand('liquid.disableFormatting', document.disable.bind(document)))
+  sub.push(registerCommand('liquid.enableFormatting', document.enable.bind(document)))
+  sub.push(registerCommand('liquid.toggleOutput', output.show()))
+  sub.push(registerCommand('liquid.formatDocument', document.document.bind(document)))
+  sub.push(registerCommand('liquid.formatSelection', document.selection.bind(document)))
 
 }
