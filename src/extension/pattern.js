6ff84b27167f95d4a5119298794a2197513496ef
Nikolas Savvidis <nicos.savidis@gmail.com>
2019-06-28 20:22:53 +0200

    Update pattern.js

--- src/extension/pattern.js
+++ src/extension/pattern.js
@@ -1,10 +1,90 @@
-import { matches } from './config'
+import Config from './config'
+import { output } from './api'
 
-/**
- * Expression Helpers
- */
-export default {
-  frontmatter: new RegExp(matches.frontmatter.join(''), 'g'),
-  tags: new RegExp(matches.tags.join(''), 'g'),
-  ignore: new RegExp(matches.ignore.join(''), 'g')
+export default class Pattern extends Config {
+
+  constructor () {
+
+    super()
+
+    this.pattern = {}
+    this.frontmatter = new RegExp(`---(?:[^]*?)---`, 'g')
+
+  }
+
+  getPatterns () {
+
+    this.getdocumentTagsPattern()
+    this.getIgnoreTagsPattern()
+
+  }
+
+  getdocumentTagsPattern () {
+
+    const language = []
+
+    for (let lang in this.config) {
+
+      this.config[lang].hasOwnProperty('tags') && this.config[lang].tags.map(({
+        type,
+        begin,
+        end
+      }) => {
+
+        if (!type) {
+
+          return output.appendLine(`💧Tag is missing "type" property`)
+
+        }
+
+        language.push(Pattern.captures(type, begin, end))
+
+      })
+
+    }
+
+    this.pattern.tags = language
+
+  }
+
+  getIgnoreTagsPattern () {
+
+    const ignore = []
+
+    this.config.html.ignored.map(({
+      type,
+      begin,
+      end
+    }) => {
+
+      if (!type) {
+
+        return output.appendLine(`💧Ignored tag is missing "type" property`)
+
+      }
+
+      ignore.push(Pattern.captures(type, begin, end))
+      //! this.matches.includes(begin) && this.matches.push(begin)
+
+    })
+
+    this.pattern.ignored = ignore
+
+  }
+
+  static captures (type, begin, end) {
+
+    const pattern = {
+
+      html: `(<(${begin})>)([^]*?)(</${end}>)`,
+      liquid: `({%-?\\s*(${begin}).*?\\s*-?%})([^]*?)({%-?\\s*${end}\\s*-?%})`
+
+    }[type]
+
+    const expression = new RegExp(pattern, 'g')
+
+    return expression
+
+  }
+
 }
