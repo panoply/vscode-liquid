a1007cf2566c0a2fedfe95b58eae6cc2bdffb442
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-09-14 14:38:16 +0200

    locales, will likely remove

---
+++ extension/providers/i18n.ts
@@ -0,0 +1,16 @@
+export const EN = {
+  ENABLED: {
+    FORMATTING: ' Enabled Liquid\'s Prettify Formatter'
+  },
+  DISABLED: {
+    FORMATTING: ' Disabled Liquid\'s Prettify Formatter'
+  },
+  SUCCESS: {
+    SELECTION: ' Selection Formatted',
+    DOCUMENT: ' Document Formatted'
+  },
+  ERRORS: {
+    SELECTION: ' Formatting Failed! The selected text is invalid or incomplete.',
+    DOCUMENT: ' Formatting Failed! Failed! The document could not be formatted, see output.'
+  }
+};
