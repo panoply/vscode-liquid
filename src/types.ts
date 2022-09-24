3f2ada37f39748f758a04b8113e33f8fef994dc4
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-09-25 01:44:18 +0200

    update types

--- src/types.ts
+++ src/types.ts
@@ -249,6 +249,66 @@ export type InLanguageIds = LiteralUnion<(
   | '[yaml]'
 ), string>
 
+/**
+ * Language document selectors
+ */
+export interface Selectors {
+  /**
+   * Liquid Language Selectors
+   */
+  active: Array<{
+    scheme: 'file';
+    language: LiteralUnion<(
+      | 'liquid'
+      | 'liquid-javascript'
+      | 'liquid-css'
+      | 'liquid-scss'
+      | 'xml'
+      | 'html'
+      | 'json'
+      | 'jsonc'
+      | 'css'
+      | 'sass'
+      | 'scss'
+      | 'less'
+      | 'jsx'
+      | 'tsx'
+      | 'javascript'
+      | 'typescript'
+    ), string>
+  }>
+  /**
+   * Liquid Language Selectors
+   */
+  liquid: Array<{
+    scheme: 'file';
+    language:
+    | 'liquid'
+    | 'liquid-javascript'
+    | 'liquid-css'
+    | 'liquid-scss'
+  }>
+  /**
+   * Extended Language Selectors
+   */
+  extend: Array<{
+    scheme: 'file';
+    language:
+    | 'xml'
+    | 'html'
+    | 'json'
+    | 'jsonc'
+    | 'css'
+    | 'sass'
+    | 'scss'
+    | 'less'
+    | 'jsx'
+    | 'tsx'
+    | 'javascript'
+    | 'typescript'
+  }>
+}
+
 export namespace Workspace {
 
   /**
