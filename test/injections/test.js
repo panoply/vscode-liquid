efdcbd8fb617a2337b29763927d54607ca30e7be
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-12-07 22:00:42 +0100

    template literal support tests

--- test/injections/test.js
+++ test/injections/test.js
@@ -1,17 +1,46 @@
+// MULTILINE WITH HIGH LIGHTING PRESERVED
+//
+liquid`
+
+{% for i in arr %}
+
+{{ foo }}
+
+{% endfor %}
+
+<div
+  foo="foo"
+ {% if condition %}
+ {{ product.available }}
+ {% endif %}></div>
+
+`;
+
+
+// ATTRIBUTE HIGH LIGHT INCLUDED
+//
 liquid`
 
 {% for i in arr %}
 
 {% endfor %}
 
+<div {% if condition %} {{ product.available }} {% endif %}></div>
+
+<div id="{% if condition %} {{ product.available }} {% endif %}"></div>
 `;
 
+// INLINE WITH HIGH LIGHTING PRESERVED
+//
+liquid`{% if condition %} {{ product.available }} {% endif %}`
+
+
+// NO HIGH LIGHTING - LITERAL BEHAVES AS INTENDED
+//
 template`
 
 {% for i in arr %}
 
 {% endfor %}
 
-`
-
-const liquid = foo
+`;
