4a432e6d6709615abe987a38f324730ad27856f9
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-12-26 04:23:36 +0100

    various util improvements and helpers

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
 import { Range, workspace, TextDocument, MarkdownString } from 'vscode';
+import prettify, { LanguageNames, Options } from '@liquify/prettify';
+import { omit, isType, has, hasPath } from 'rambdax';
+import stripJsonComments from 'strip-json-comments';
+import { InLanguageIds, LanguageIds, Liquidrc } from './types';
+import { FormatStatus } from 'providers/StatusBarItem';
+import parseJSON from 'parse-json';
 import { access } from 'node:fs/promises';
 import { join } from 'node:path';
 import os from 'node:os';
-import prettify, { LanguageNames } from '@liquify/prettify';
-import { omit, isType, has } from 'rambdax';
-import stripJsonComments from 'strip-json-comments';
-import { InLanguageIds, LanguageIds, Liquidrc, Workspace } from './types';
-import parseJSON from 'parse-json';
-import { Status } from 'providers/StatusBarItem';
 
 /* -------------------------------------------- */
 /* CONSTANTS                                    */
@@ -95,11 +95,7 @@ export async function pathExists (path: string) {
  * Omits the `ignore` and `enable` options
  * from the extended format configurations
  */
-export const rulesNormalize = omit([
-  'ignore',
-  'enable',
-  'languages'
-]);
+export const rulesNormalize = omit([ 'ignore' ]);
 
 /**
  * Omit Prettify Rules
@@ -120,6 +116,21 @@ export const rulesOmitted = omit([
 /* -------------------------------------------- */
 /* FUNCTIONS                                    */
 /* -------------------------------------------- */
+
+export function multiline (...input: string[]) {
+
+  return input.join(' ');
+
+}
+
+/**
+ * Uppercase First Letter of string
+ */
+export function upcase (string: string) {
+
+  return string[0].toUpperCase() + string.slice(1);
+
+}
 
 /**
  * Parse Error Stack
@@ -176,19 +187,26 @@ export function mdString (text: string | string[]) {
  * Strips comments from JSONC files and returns the parsed
  * object result or throws an information error.
  */
-export function jsonc (input: object | string): Liquidrc {
+export function jsonc (input: string): Liquidrc {
 
   const json = isString(input) ? input : JSON.stringify(input);
 
-  return parseJSON(stripJsonComments(json as string));
+  return parseJSON(stripJsonComments(json));
 
 }
 
-export function getDefaultFormatter (language: string) {
+/**
+ * Get Default Formatter
+ *
+ * Returns the name of the currently defined formatter
+ * for the provided language id or `undefined` if none.
+ */
+export function getDefaultFormatter (language: InLanguageIds): string {
 
   const liquid = workspace.getConfiguration('liquid');
   const target = liquid.has('settings.target')
-    ? liquid.get('settings.target') === 'workspace' ? 'workspaceValue' : 'globalValue'
+    ? liquid.get('settings.target') === 'workspace'
+      ? 'workspaceValue' : 'globalValue'
     : 'workspaceValue';
 
   const setting = workspace.getConfiguration().inspect(language);
@@ -208,12 +226,29 @@ export function getDefaultFormatter (language: string) {
  */
 export function isDefaultFormatter (language: InLanguageIds, target: 'workspaceValue' | 'globalValue') {
 
-  const setting = workspace.getConfiguration().inspect(language);
+  const setting = workspace.getConfiguration().inspect(`[${language}]`);
 
   if (!isObject(setting[target])) return false;
   if (!has('editor.defaultFormatter', setting[target])) return false;
 
   return setting[target]['editor.defaultFormatter'] === 'sissel.shopify-liquid';
+
+}
+
+/**
+ * Is Format on Save
+ *
+ * Whether or not the provided in~language setting has defined
+ * `formatOnSave` as enabled or not.
+ */
+export function isFormatOnSave (language: InLanguageIds, target: 'workspaceValue' | 'globalValue') {
+
+  const setting = workspace.getConfiguration().inspect(`[${language}]`);
+
+  if (!isObject(setting[target])) return false;
+  if (!has('editor.defaultFormatter', setting[target])) return false;
+
+  return setting[target]['editor.formatOnSave'];
 
 }
 
@@ -227,22 +262,84 @@ export function hasDeprecatedSettings (rcfile: Liquidrc = undefined) {
 
   if (rcfile === undefined) {
 
-    const liquid = workspace.getConfiguration('liquid');
+    const liquid = workspace.getConfiguration().inspect<any>('liquid');
 
-    if (liquid.has('format')) {
-      const format = liquid.get<boolean>('format');
-      return (isBoolean(format) && format === true);
+    // These workspace configurations determine that the user
+    // is using configuration pre v3.0.0 - This is very old configurations
+    if (
+      has('rules', liquid.workspaceValue) || (
+        has('format', liquid.workspaceValue) &&
+        isBoolean(liquid.workspaceValue.format)
+      )
+    ) {
+
+      return '3.0.0';
+
     }
 
-    return false;
+    // These workspace configuration determine that the user has
+    // settings post v3.0.0 but pre v3.4.0 - This is most recent conigurations
+    if (
+      has('format', liquid.workspaceValue) && (
+        has('enable', liquid.workspaceValue.format) ||
+        has('wrap', liquid.workspaceValue.format) ||
+        has('markup', liquid.workspaceValue.format) ||
+        has('crlf', liquid.workspaceValue.format) ||
+        has('preserveLine', liquid.workspaceValue.format) ||
+        has('indentSize', liquid.workspaceValue.format) ||
+        has('style', liquid.workspaceValue.format) ||
+        has('script', liquid.workspaceValue.format) ||
+        has('json', liquid.workspaceValue.format) ||
+        has('endNewline', liquid.workspaceValue.format) ||
+        has('commentIndent', liquid.workspaceValue.format)
+      )
+    ) {
+
+      return '3.4.0';
+
+    };
+
+    return null;
   }
 
-  return rcfile && (
-    has('html', rcfile) ||
-    has('js', rcfile) ||
-    has('css', rcfile) ||
-    has('scss', rcfile)
-  );
+  if (rcfile) {
+
+    // These workspace configurations determine that the user
+    // is using configuration pre v3.0.0 - This is very old configurations
+    if (
+      has('html', rcfile) ||
+      has('js', rcfile) ||
+      has('css', rcfile) ||
+      has('scss', rcfile) ||
+      has('js', rcfile) || (has('ignore', rcfile) && isObject((rcfile as any).ignore[0]))) {
+
+      return '3.0.0';
+
+    }
+
+    // These workspace configuration determine that the user has
+    // settings post v3.0.0 but pre v3.4.0 - This is most recent conigurations
+
+    if (
+      has('ignore', rcfile) ||
+      has('wrap', rcfile) ||
+      has('markup', rcfile) ||
+      has('style', rcfile) ||
+      has('script', rcfile) ||
+      has('json', rcfile) ||
+      has('crlf', rcfile) ||
+      has('preserveLine', rcfile) ||
+      has('indentSize', rcfile) ||
+      has('endNewline', rcfile) ||
+      has('commentIndent', rcfile)) {
+
+      return '3.4.0';
+    }
+
+  }
+
+  return null;
+
 }
 
 /**
@@ -291,20 +388,22 @@ export function isFile (path: string, matchFile: string) {
  * Returns a string value of the `Status` enum that
  * will infer a method on the `StatusBarItem` class.
  */
-export function getStatusBar (status: Status) {
+export function getStatusBar (status: FormatStatus) {
 
   switch (status) {
-    case Status.Enabled:
+    case FormatStatus.Enabled:
       return 'enable';
-    case Status.Disabled:
+    case FormatStatus.Disabled:
       return 'disable';
-    case Status.Hidden:
+    case FormatStatus.NotDefault:
       return 'hide';
-    case Status.Error:
+    case FormatStatus.Hidden:
+      return 'hide';
+    case FormatStatus.Error:
       return 'error';
-    case Status.Loading:
+    case FormatStatus.Loading:
       return 'loading';
-    case Status.Ignoring:
+    case FormatStatus.Ignoring:
       return 'ignore';
   }
 
@@ -429,12 +528,180 @@ export function getTime (brace = true) {
 }
 
 /**
+ * Update Deprecated Rules
+ *
+ * Executes an automated update of .liquidrc and/or workspace
+ * settings structures.
+ */
+export function updateRules (rules: any) {
+
+  const newRules = {
+    wrap: hasPath('wrap', rules)
+      ? rules.wrap
+      : 0,
+    crlf: hasPath('crlf', rules)
+      ? rules.crlf
+      : false,
+    indentSize: hasPath('indentSize', rules)
+      ? rules.indentSize
+      : 2,
+    preserveLine: hasPath('preserveLine', rules)
+      ? rules.preserveLine
+      : 2,
+    endNewline: hasPath('endNewline', rules)
+      ? rules.endNewline
+      : false,
+    liquid: {
+      commentIndent: hasPath('commentIndent', rules)
+        ? rules.commentIndent
+        : true,
+      commentNewline: hasPath('markup.commentNewline', rules)
+        ? rules.markup.commentNewline
+        : true,
+      delimiterTrims: hasPath('markup.delimiterTrims', rules)
+        ? rules.markup.delimiterTrims
+        : 'preserve',
+      ignoreTagList: [],
+      lineBreakSeparator: hasPath('markup.lineBreakSeparator', rules)
+        ? rules.markup.lineBreakSeparator
+        : 'before',
+      normalizeSpacing: hasPath('markup.normalizeSpacing', rules)
+        ? rules.markup.normalizeSpacing
+        : true,
+      preserveComment: false,
+      quoteConvert: 'single',
+      valueForce: 'intent'
+    },
+    markup: {
+      correct: false,
+      quoteConvert: 'double',
+      selfCloseSpace: true,
+      commentNewline: hasPath('markup.commentNewline', rules)
+        ? rules.markup.commentNewline
+        : true,
+      commentIndent: hasPath('markup.commentIndent', rules)
+        ? rules.commentIndent
+        : true,
+      forceIndent: hasPath('markup.forceIndent', rules)
+        ? rules.commentIndent
+        : true,
+      ignoreScripts: false,
+      ignoreStyles: false,
+      attributeSort: hasPath('markup.attributeSort', rules)
+        ? rules.markup.attributeSort
+        : false,
+      attributeSortList: hasPath('markup.attributeSortList', rules)
+        ? rules.markup.attributeSortList
+        : [],
+      attributeCasing: 'preserve',
+      forceAttribute: 2,
+      forceLeadAttribute: hasPath('markup.forceLeadAttribute', rules)
+        ? rules.markup.forceLeadAttribute
+        : true,
+      preserveAttributes: hasPath('markup.preserveAttributes', rules)
+        ? rules.markup.preserveAttributes
+        : false,
+      preserveText: hasPath('markup.preserveText', rules)
+        ? rules.markup.preserveText
+        : false,
+      preserveComment: hasPath('markup.preserveComment', rules)
+        ? rules.markup.preserveComment
+        : false
+    },
+    json: {
+      bracePadding: false,
+      braceAllman: true,
+      arrayFormat: 'indent',
+      objectIndent: 'indent',
+      objectSort: false
+    },
+    style: {
+      correct: true,
+      commentNewline: hasPath('markup.commentNewline', rules)
+        ? rules.markup.commentNewline
+        : true,
+      commentIndent: hasPath('markup.commentIndent', rules)
+        ? rules.commentIndent
+        : true,
+      sortProperties: false,
+      sortSelectors: false,
+      noLeadZero: true,
+      quoteConvert: 'single',
+      classPadding: true,
+      atRuleSpace: true
+    },
+    script: {
+      correct: true,
+      arrayFormat: 'indent',
+      objectIndent: 'indent',
+      braceAllman: false,
+      methodChain: 3,
+      caseSpace: true,
+      endComma: 'never',
+      quoteConvert: 'single',
+      elseNewline: true,
+      functionNameSpace: true,
+      functionSpace: true,
+      braceNewline: false,
+      braceStyle: 'none',
+      commentNewline: true,
+      neverFlatten: false,
+      noCaseIndent: true,
+      noSemicolon: false,
+      objectSort: false,
+      vertical: false
+    }
+  };
+
+  for (const rule in newRules) {
+
+    if (rule === 'markup') {
+
+      for (const mu in newRules[rule]) {
+        if (newRules[rule][mu] === prettify.options.rules.markup[mu]) {
+          delete newRules[rule][mu];
+        }
+      }
+
+    } else if (rule === 'json') {
+
+      for (const jn in newRules[rule]) {
+        if (newRules[rule][jn] === prettify.options.rules.json[jn]) {
+          delete newRules[rule][jn];
+        }
+      }
+
+    } else if (rule === 'style') {
+
+      for (const st in newRules[rule]) {
+        if (newRules[rule][st] === prettify.options.rules.markup[st]) {
+          delete newRules[rule][st];
+        }
+      }
+
+    } else if (rule === 'script') {
+
+      for (const sc in newRules[rule]) {
+        if (newRules[rule][sc] === prettify.options.rules.markup[sc]) {
+          delete newRules[rule][sc];
+        }
+      }
+
+    }
+
+  }
+
+  return newRules;
+
+}
+
+/**
  * Merge Preferences
  *
  * Extracts the users preference settings and returns
  * a Prettify model that will be merged with defaults.
  */
-export function rulesDefault (): Workspace.Format {
+export function rulesDefault (): Options {
 
   const rules = rulesOmitted(prettify.options.rules);
   const editor = workspace.getConfiguration('editor');
@@ -442,23 +709,29 @@ export function rulesDefault (): Workspace.Format {
   rules.json.braceAllman = true;
 
   return {
-    ignore: [],
     wrap: editor.get<number>('wordWrapColumn') || rules.wrap,
-    commentIndent: rules.commentIndent,
     crlf: rules.crlf,
     indentSize: editor.get<number>('tabSize') || rules.indentSize,
-    preserveComment: rules.preserveComment,
     preserveLine: rules.preserveLine,
     endNewline: editor.get<boolean>('renderFinalNewline') || rules.endNewline,
+    liquid: {
+      commentIndent: rules.liquid.commentIndent,
+      commentNewline: rules.liquid.commentNewline,
+      delimiterTrims: rules.liquid.delimiterTrims,
+      ignoreTagList: rules.liquid.ignoreTagList,
+      lineBreakSeparator: rules.liquid.lineBreakSeparator,
+      normalizeSpacing: rules.liquid.normalizeSpacing,
+      preserveComment: rules.liquid.preserveComment,
+      quoteConvert: rules.liquid.quoteConvert,
+      valueForce: rules.liquid.valueForce
+    },
     markup: {
       correct: rules.markup.correct,
       quoteConvert: rules.markup.quoteConvert,
       selfCloseSpace: rules.markup.selfCloseSpace,
       commentNewline: rules.markup.commentNewline,
+      commentIndent: rules.markup.commentIndent,
       forceIndent: rules.markup.forceIndent,
-      lineBreakSeparator: rules.markup.lineBreakSeparator,
-      delimiterTrims: rules.markup.delimiterTrims,
-      valueForce: rules.markup.valueForce,
       attributeSort: rules.markup.attributeSort,
       attributeSortList: rules.markup.attributeSortList,
       attributeCasing: rules.markup.attributeCasing,
@@ -467,8 +740,7 @@ export function rulesDefault (): Workspace.Format {
       preserveAttributes: rules.markup.preserveAttributes,
       preserveText: rules.markup.preserveText,
       ignoreScripts: rules.markup.ignoreScripts,
-      ignoreStyles: rules.markup.ignoreStyles,
-      normalizeSpacing: rules.markup.normalizeSpacing
+      ignoreStyles: rules.markup.ignoreStyles
     },
     json: {
       bracePadding: rules.json.bracePadding,
@@ -484,7 +756,7 @@ export function rulesDefault (): Workspace.Format {
       noLeadZero: rules.style.noLeadZero,
       quoteConvert: rules.style.quoteConvert,
       classPadding: rules.style.classPadding,
-      compressCSS: rules.style.compressCSS
+      atRuleSpace: rules.style.atRuleSpace
     },
     script: {
       correct: rules.script.correct,
@@ -519,74 +791,72 @@ export function rulesDefault (): Workspace.Format {
  *
  * Applies the recommended beautification rules
  */
-export function rulesRecommend (): Workspace.Format {
+export function rulesRecommend (): Liquidrc {
 
   return {
-    ignore: [],
-    wrap: 0,
-    commentIndent: true,
-    crlf: false,
-    indentSize: 2,
-    preserveComment: false,
-    preserveLine: 2,
-    endNewline: true,
-    markup: {
-      correct: false,
-      quoteConvert: 'double',
-      selfCloseSpace: true,
-      commentNewline: true,
-      forceIndent: false,
-      attributeSort: true,
-      attributeSortList: [],
-      delimiterTrims: 'preserve',
-      attributeCasing: 'preserve',
-      lineBreakSeparator: 'before',
-      normalizeSpacing: true,
-      valueForce: 'intent',
-      forceAttribute: 3,
-      forceLeadAttribute: false,
-      preserveAttributes: false,
-      preserveText: false
-    },
-    json: {
-      bracePadding: false,
-      braceAllman: true,
-      arrayFormat: 'indent',
-      objectIndent: 'indent',
-      objectSort: false
-    },
-    style: {
-      correct: false,
-      sortProperties: true,
-      sortSelectors: true,
-      noLeadZero: true,
-      quoteConvert: 'single',
-      classPadding: true,
-      compressCSS: false
-    },
-    script: {
-      correct: true,
-      arrayFormat: 'indent',
-      objectIndent: 'indent',
-      braceAllman: false,
-      methodChain: 3,
-      caseSpace: true,
-      endComma: 'never',
-      quoteConvert: 'single',
-      elseNewline: true,
-      functionNameSpace: true,
-      functionSpace: true,
-      ternaryLine: true,
-      braceNewline: false,
-      bracePadding: false,
-      braceStyle: 'none',
-      commentNewline: true,
-      neverFlatten: false,
-      noCaseIndent: true,
-      noSemicolon: false,
-      objectSort: false,
-      vertical: false,
-      variableList: 'none'
+    format: {
+      wrap: 0,
+      commentIndent: true,
+      crlf: false,
+      indentSize: 2,
+      preserveComment: false,
+      preserveLine: 2,
+      endNewline: true,
+      liquid: {
+        commentIndent: true,
+        commentNewline: true,
+        delimiterTrims: 'tags',
+        lineBreakSeparator: 'before',
+        normalizeSpacing: true,
+        quoteConvert: 'single',
+        valueForce: 'intent',
+        ignoreTagList: []
+      },
+      markup: {
+        quoteConvert: 'double',
+        selfCloseSpace: true,
+        commentNewline: true,
+        forceIndent: true,
+        commentIndent: true,
+        ignoreScripts: true,
+        ignoreStyles: false,
+        forceAttribute: 3,
+        forceLeadAttribute: true
+      },
+      json: {
+        bracePadding: false,
+        braceAllman: true,
+        arrayFormat: 'indent',
+        objectIndent: 'indent',
+        objectSort: false
+      },
+      style: {
+        correct: true,
+        noLeadZero: true,
+        quoteConvert: 'single',
+        atRuleSpace: true
+      },
+      script: {
+        correct: true,
+        arrayFormat: 'indent',
+        objectIndent: 'indent',
+        braceAllman: false,
+        methodChain: 3,
+        caseSpace: true,
+        endComma: 'never',
+        quoteConvert: 'single',
+        elseNewline: true,
+        functionNameSpace: true,
+        functionSpace: true,
+        braceNewline: false,
+        braceStyle: 'none',
+        commentNewline: true,
+        neverFlatten: false,
+        noCaseIndent: true,
+        noSemicolon: false,
+        objectSort: false,
+        vertical: false
+      }
     }
   };
 
