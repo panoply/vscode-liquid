6878adbc376a25837e32737c96f44307c7714dec
Nikolas Savvidis <nicos.savidis@gmail.com>
2023-02-17 00:55:31 +0100

    activation implementation for variables

--- src/providers.ts
+++ src/providers.ts
@@ -3,11 +3,20 @@ import { CompletionProvider } from 'providers/CompletionProvider';
 import { DocumentLinkProvider } from 'providers/DocumentLinkProvider';
 import { HoverProvider } from 'providers/HoverProvider';
 import { CodeLensProvider } from 'providers/CodeLensProvider';
-import { Extension } from 'extension';
 import { Engine, liquid, q } from '@liquify/liquid-language-specs';
-import { CompletionItemKind, workspace } from 'vscode';
-import { entries } from 'utils';
-import { getFilterCompletions, getLogicalCompletions, getObjectCompletions, getObjectKind, getTagCompletions } from 'lexical/parse';
+import { CompletionItemKind } from 'vscode';
+import { entries, parseJsonFile } from 'utils';
+import { operators } from 'completions/helpers/operators';
+import {
+  getFilterCompletions,
+  getItemKind,
+  getOperatorCompletions,
+  getObjectCompletions,
+  getFileCompletions,
+  getSchemaCompletions,
+  getTagCompletions
+} from 'lexical/tags';
+import { Extension } from 'extension';
 
 /**
  * Providers
@@ -58,58 +67,85 @@ export class Providers extends Extension {
   public codelens = new CodeLensProvider();
 
   /**
-   * Generate Data
-   *
-   * Constructs and generates data references used in providers
+   * Get Completions
    */
   async getContext () {
 
     if (this.engine === 'shopify') {
+
       q.setEngine(Engine.shopify);
+
+      if (this.completion.enable.operators) {
+        this.completion.items.set('operators', getOperatorCompletions(operators));
+      }
+
+      if (this.completion.enable.tags) {
+        this.completion.items.set('tags', getTagCompletions(liquid.shopify.tags));
+      }
+
+      if (this.completion.enable.filters) {
+        this.completion.items.set('filters', getFilterCompletions(liquid.shopify.filters));
+      }
+
+      if (this.completion.enable.objects) {
+        this.completion.items.set('objects', getObjectCompletions(liquid.shopify.objects));
+      }
+
+      if (this.completion.enable.snippets) {
+        this.completion.items.set('snippets', getFileCompletions(this.files.snippets));
+      }
+
+      if (this.completion.enable.sections) {
+        this.completion.items.set('sections', getFileCompletions(this.files.sections));
+      }
+
     }
-
-    if (this.uri.files.locales !== null) {
-      const locales = await workspace.fs.readFile(this.uri.files.locales);
-      this.completion.items.locales = JSON.parse(locales.toString());
-    }
-
-    if (this.uri.files.settings !== null) {
-      const settings = await workspace.fs.readFile(this.uri.files.settings);
-      this.completion.items.settings = JSON.parse(settings.toString());
-    }
-
-    if (this.uri.files.snippets !== null) {
-      // this.complete.sections = this.uri.files.sections.map(({ fsPath }) => (<CompletionItem>{
-      //   label: basename(fsPath, '.liquid'),
-      //   insertText: basename(fsPath, '.liquid'),
-      //   kind: CompletionItemKind.Reference,
-      //   documentation: new MarkdownString(`Snippet File\n\n---\n\n[${basename(fsPath)}](${fsPath})`)
-      // }));
-
-    }
-    // if (this.files.snippets !== null) {
-
-    //   this.complete.locales.file = this.files.locales;
-    //   this.complete.locales.items = JSON.parse(readFileSync(this.files.locales).toString());
-    // }
-
-    if (this.completion.enable.logical) {
-      this.completion.items.logical = getLogicalCompletions();
-    }
-
-    if (this.completion.enable.tags) {
-      this.completion.items.tags = entries(liquid.shopify.tags).map(getTagCompletions);
-    }
-
-    if (this.completion.enable.filters) {
-      this.completion.items.filters = entries(liquid.shopify.filters).map(getFilterCompletions);
-    }
-
-    if (this.completion.enable.objects) {
-      this.completion.items.objects = entries(liquid.shopify.objects).map(getObjectCompletions);
-      this.completion.items.common = getObjectKind(this.completion.items.objects, [ CompletionItemKind.Module ]);
-    }
-
   }
+
+  /**
+   * Generate Data
+   *
+   * Constructs and generates data references used in providers
+   */
+  // async getContexts () {
+
+  //   if (this.engine === 'shopify') {
+
+  //     q.setEngine(Engine.shopify);
+
+  //     if (this.uri.files.locales !== null) {
+
+  //       const items = await parseJsonFile(this.uri.files.locales);
+
+  //       if (items !== null) {
+  //         this.completion.files.locales.items = items;
+  //         this.completion.files.locales.path = this.uri.files.locales.fsPath;
+  //       }
+  //     }
+
+  //     if (this.uri.files.settings !== null) {
+  //       this.completion.files.settings = await parseJsonFile(this.uri.files.settings);
+  //     }
+
+  //     if (this.completion.enable.logical) {
+  //       this.completion.items.logical = getLogicalCompletions();
+  //     }
+
+  //     if (this.completion.enable.tags) {
+  //       this.completion.items.tags = entries(liquid.shopify.tags).map(getTagCompletions);
+  //     }
+
+  //     if (this.completion.enable.filters) {
+  //       this.completion.items.filters = entries(liquid.shopify.filters).map(getFilterCompletions);
+  //     }
+
+  //     if (this.completion.enable.objects) {
+  //       this.completion.items.objects = entries(liquid.shopify.objects).map(getObjectCompletions);
+  //       this.completion.items.common = getObjectKind(this.completion.items.objects, [ CompletionItemKind.Module ]);
+  //     }
+
+  //   }
+
+  // }
 
 }
