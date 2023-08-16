1f6fcdc2ac9b673054643c9170d28abfb2753370
Nikolas Savvidis <nicos@gmx.com>
2023-08-16 07:16:51 +0200

    cursor token detection for translation args

--- extension/parse/tokens.ts
+++ extension/parse/tokens.ts
@@ -35,11 +35,15 @@ export interface IToken {
    */
   get tagName(): string;
   /**
+   * The last known locale reference, defaults to `null` if no locale object
+   */
+  get locale(): string;
+  /**
    * The last known filter (if any) on the token, defaults to `null`
    */
   get filter(): string;
   /**
-   * The last known filter (if any) on the token, defaults to `null`
+   * The last known object property (if any) on the token, defaults to `null`
    */
   get object(): string;
 }
@@ -133,7 +137,7 @@ export function getTokenScope () {
 
 }
 
-export function getLiquidTokenCursor ({ text, offset }: IToken, vars: Complete.Vars): Token {
+export function getLiquidTokenCursor ({ text, offset, filter }: IToken, vars: Complete.Vars): Token {
 
   /**
    * Obtain the text portion
@@ -172,15 +176,26 @@ export function getLiquidTokenCursor ({ text, offset }: IToken, vars: Complete.V
 
   // if not null, we have a match
   if (last === Char.PIP) {
+
     if (q.setTag(tagName)) {
+
       if ($.liquid.tag.filters) return Token.LiquidTagFilterTag;
+
     } else {
+
       return Token.LiquidTagFilterObject;
+
     }
+
   } else if (last === Char.COL) {
+
     return Token.LiquidTagArgument;
+
   } else if (last === Char.DOT) {
-    return /["'][^'"]*?$/.test(text.slice(2, offset - 1)) ? Token.Locale : Token.Property;
+
+    return /["'][^'"]*?$/.test(text.slice(2, offset - 1))
+      ? Token.Locale
+      : Token.Property;
   }
 
   let tagMatch: string;
@@ -238,7 +253,7 @@ export function getLiquidTokenCursor ({ text, offset }: IToken, vars: Complete.V
  * This is a series of validation checks which is used to control the
  * type of completion to be provided based on the surrounding content.
  */
-export function getTokenCursor ({ text, offset, tagName }: IToken, vars: Complete.Vars): Token {
+export function getTokenCursor ({ text, offset, tagName, filter }: IToken, vars: Complete.Vars): Token {
 
   /**
    * Previous character excluding whitespace
@@ -256,6 +271,8 @@ export function getTokenCursor ({ text, offset, tagName }: IToken, vars: Complet
     return Token.Argument;
   } else if (last === Char.DOT) {
     return /["'][^'"]*?$/.test(text.slice(2, offset - 1)) ? Token.Locale : Token.Property;
+  } else if (filter === 't' && last === Char.COM) {
+    return Token.LocaleArgument;
   }
 
   /**
@@ -367,9 +384,37 @@ export function getToken (content: string, index: number): IToken {
     ender,
     text,
     offset,
+    get locale () {
+
+      if (type !== Token.Object) return null;
+
+      const before = text.search(/(?<=^{{-?\s*["'])/);
+
+      if (before > -1) {
+        const object = text.slice(before, offset).trim();
+        return object.endsWith('.') ? object.slice(0, -1) : object.slice(1);
+      }
+
+      return null;
+
+    },
     get object () {
 
-      const object = text.slice(text.lastIndexOf(' ', offset - 1), offset).trim();
+      let object: string = '';
+      let before = text.lastIndexOf(' ', offset - 1);
+
+      if (before > -1) {
+
+        object = text.slice(before, offset).trim();
+
+      } else if (type === Token.Object) {
+
+        // Check for locale
+        before = text.search(/(?<=^{{-?["'])/);
+
+        if (before - 1) object = text.slice(before, offset).trim();
+
+      }
 
       return object.charCodeAt(0) === Char.DQO || object.charCodeAt(0) === Char.SQO
         ? object.slice(1)
